ebd766c1fa0d3ae50df5e52204b9d07dc846d519
James Almeida <james.almeida.ti@gmail.com>
2018-08-27 19:11:22 -0300

    Configurar conexao mongodb

--- catalogo.py
+++ catalogo.py
@@ -1,6 +1,13 @@
 from flask import Flask
 from flask import jsonify
 from flask import request
+from pymongo import MongoClient
 app = Flask(__name__)
+
+client = MongoClient()
+db = client.teste_db
+collection = db.catalogo
+
+
 
 
